a0c11555d413de960e562d41a19920174ef9f3dc
Mishabich42 <dzipolovc@gmail.com>
2023-01-22 15:08:53 +0200

    Homework

---
+++ res.py
@@ -0,0 +1,19 @@
+import colorama
+# вимикає згортання
+colorama.init(wrap=False)
+# автоскидання кольору, колір заднього плану, яскравість тексту
+colorama.init(autoreset=True)
+# колір тексту
+print(colorama.Fore.MAGENTA + 'some MAGENTA text')
+# колір заднього планну тексту
+print(colorama.Back.BLACK + 'and with a green background')
+# яскравість тексту
+print(colorama.Style.DIM + 'and in dim text')
+# скидання кольору, колір заднього плану, яскравість тексту
+print(colorama.Style.RESET_ALL)
+print('back to normal now')
+print(colorama.Fore.MAGENTA + colorama.Back.BLACK + colorama.Style.DIM + "back")
+
+
+
+
