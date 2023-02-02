377b2a8906c26ac29ec198599830407d5ed19db4
Mishabich42 <dzipolovc@gmail.com>
2023-02-02 18:51:51 +0200

    Homework я сподіваюсь що  правильно зрозумів завдання

---
+++ ку.py
@@ -0,0 +1,40 @@
+# Task 1
+""""
+my_iterable = iter([i for i in range(10000000000000000000000)])
+
+for item in my_iterable:
+    print(next(my_iterable))
+
+"""
+
+
+# Task 2
+
+def calculate(expression):
+    def calculate(*args, **kwargs):
+        try:
+            result = int(expression(*args, **kwargs))
+        except ValueError:
+            print("You must enter digit")
+        except IndexError:
+            print("IndexError")
+        except ZeroDivisionError:
+            print("Сannot be divided by 0")
+        except SyntaxError:
+            print("You must enter (+, -, *, /)")
+        except TypeError:
+            print("You must enter (+, -, *, /)")
+        except NameError:
+            print("You must enter digit")
+        else:
+            print(result)
+
+    return calculate
+
+
+@calculate
+def caluculater(exprassion):
+    return eval(exprassion)
+
+
+caluculater(input(""))
