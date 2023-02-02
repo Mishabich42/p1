adc1ecf62f190ff8b2fd94a67e3fce8d19418e2a
Mishabich42 <dzipolovc@gmail.com>
2023-02-02 18:33:11 +0200

    Homework

--- test.py
+++ test.py
@@ -2,22 +2,25 @@ import random as r
 from datetime import datetime
 
 
-def First_function(a=r.randint(1, 100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000)):
-    try:
-        b = int(input("Print any digit \n"))
-        if a == b:
+def First_function(number):
+    def First_function(*args, **kwargs):
+        result = number(*args, **kwargs)
+        a = r.randint(1,10)
+        if a == result:
             print("You win")
         else:
             print("You looser")
-    except ValueError:
-        print("You must print digit")
-    except IndexError:
-        print("IndexError")
-    finally:
-        pass
+    return First_function
+
+@First_function
 
 def second_function():
-    start_time = datetime.now()
-    First_function()
-    end_time = datetime.now()
-    print('Duration: {}'.format(end_time - start_time))
+    try:
+        start_time = datetime.now()
+        First_function(int(input("Enter any digit \n")))
+        end_time = datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
+    except ValueError:
+        print("You must enter digit")
+    except IndexError:
+        print("IndexError")
