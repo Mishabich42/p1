3958c436647c086d54eaff933fd5d3a614011f82
Mishabich42 <113230213+Mishabich42@users.noreply.github.com>
2023-08-12 19:27:46 +0300

    Delete yt.py

--- yt.py
+++
@@ -1,9 +0,0 @@
-from pytube import YouTube
-
-yt = YouTube('https://www.youtube.com/watch?v=zhhWCRyP6M0') #ссылка на видео.
-# yt.stream показывает какое видео ты можешь скачать
-# (mp4(720) + audio или только mp4(1080) без звука).
-# Сейчас стоит фильтр по mp4.
-print(yt.streams.filter(file_extension='mp4'))
-stream = yt.streams.get_by_itag(22) #выбираем по тегу, в каком формате будем скачивать.
-stream.download() #загружаем видео.
