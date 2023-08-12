746a721d04af2e1405c7d65f088e4a1e2852bc7c
Mishabich42 <113230213+Mishabich42@users.noreply.github.com>
2023-08-12 19:22:21 +0300

    Delete p2.py

--- p2.py
+++
@@ -1,8 +0,0 @@
-class Student:
-    print("Hi")
-    def __init__(self,height=160):
-        self.height = height
-        print("L'm life")
-first_Student = Student()
-print(first_Student.height)
-maks = Student(height=240)
